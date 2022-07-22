a1401751bab1826d003d9ee22796b976e1057860
marcio-eduardo <marcioeduardo.ti@gmail.com>
2022-07-22 17:12:41 -0300

    Aula-2

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,9 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      home: unsdefined;
+      new: undefined;
+      details: { orderId: string};
+    }
+  }
+}
